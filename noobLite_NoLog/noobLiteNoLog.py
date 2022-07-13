14c07dd8e8f6462def6a598e8b01ee907be2ac1c
Dusty <12129838+dusty-dusty@users.noreply.github.com>
2022-07-12 21:12:16 -0600

    Add files via upload

--- noobLite_NoLog/noobLiteNoLog.py
+++ noobLite_NoLog/noobLiteNoLog.py
@@ -3,12 +3,12 @@
 
 # *change one of the following to True*
 
-useMouse = True  # will use mouse and right click to stop from logging out. Might be bugged if game not on main monitor
-useArrow = False  # will use arrow keys to stop from logging out
-useType = False  # will type to stop from logging out
+useMouse = False  # will use mouse and right click to stop from logging out. Might be bugged if game not on main monitor
+useArrowKey = False  # will use arrow keys to stop from logging out
+useTyping = False  # will type to stop from logging out
 useFKey = False  # will use 2 fkeys to stop from logging out
 
-whatToType = 'enter', 'a', 'backspace', 'backspace'  # what to type if using useType you can edit what you want here, eg "f, backspace" or "shift+s, backspace"
+whatToType = 'enter', 'a', 'backspace', 'backspace'  # what to type if using useTyping you can edit what you want here, eg "f, backspace" or "shift+s, backspace"
 fkeys = 'f2', 'f1'  # what fkeys you want to press *in this order* if using f keys, eg 'f3', 'f2', 'f3'. You want 2 min values
 
 # if your rs name is in the top left of runelite window please write your name here exactly like it is on the game window
@@ -16,17 +16,61 @@ name = 'yourNameHERE'
 
 # Only change ABOVE this text. Unless you know what you are doing
 
-
+import sqlite3
 from time import sleep, time
 from win32gui import GetWindowText, GetForegroundWindow, GetWindowRect, FindWindow, SetForegroundWindow, \
     ShowWindow, GetWindowPlacement, SetActiveWindow
 from win32con import SW_SHOW, SW_RESTORE
+from os import system
 from random import randint, uniform, choice
-from keyboard import send, press as kpress, release as krelease, on_release, unhook_all
-from mouse import get_position, move, right_click, on_button, unhook_all as mouse_unhook_all
+from keyboard import send, press as kpress, release as krelease, on_release
+from mouse import get_position, move, right_click, on_button
 
 forceStop = False
 start = None
+
+
+def makeDB():
+    conn = sqlite3.connect(f'settings.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE pickChoice
+                   (previousPick integer)
+                   ''')
+    c.execute('''CREATE TABLE fKeys
+                       (typeFKeys text)
+                       ''')
+    c.execute('''CREATE TABLE typeKeys
+                           (typeMsgKeys text)
+                           ''')
+    conn.commit()
+    conn.close()
+
+
+def readUsed():
+    sqConn = sqlite3.connect(f'settings.db')
+    cursor = sqConn.cursor()
+    data = cursor.execute("""SELECT previousPick FROM pickChoice""").fetchall()
+    output1 = data[0][0]
+    output2 = data[1][0]
+    cursor.close()
+    return output1, output2
+
+
+def updateUsed(updateInt, updateText, oldInt, oldText):
+    sqConn = sqlite3.connect(f'settings.db')
+    cursor = sqConn.cursor()
+    cursor.executemany(f""" UPDATE pickChoice 
+                        set previousPick = ? 
+                        where previousPick = ?""", [(updateInt, oldInt), (updateText, oldText)])
+    sqConn.commit()
+    cursor.close()
+
+
+#a, b = readUsed()
+# print(a, b)
+#updateUsed(0, 'None', a, b)
+# a, b = readUsed()
+# print(a, b)
 
 
 def getWP(windowId):
@@ -34,7 +78,7 @@ def getWP(windowId):
 
 
 def setWindow(windowId):
-    #print(windowId)
+    # print(windowId)
     if getWP(windowId) == 2:
         ShowWindow(windowId, SW_RESTORE)
     else:
@@ -45,24 +89,122 @@ def setWindow(windowId):
 
 def timeReset():
     global start
-    mouse_unhook_all()
-    unhook_all()
+    # mouse_unhook_all()
+    # unhook_all()
     currentWindow = GetWindowText(GetForegroundWindow())
     if 'runelite' in currentWindow.lower():
         start = time()
-        # print("reset time")
+        # print("reset time ", randint(0, 100))
     # else:
     # print("wrong window")
+
+def startUI():
+    global useMouse, useArrowKey, useTyping, useFKey
+    prePickValue, prePickText = readUsed()
+
+    if prePickValue == 0:
+        changeUI()
+
+    picker = None
+    try:
+        # quality = readQuality()
+        # print(f'Made by: Dusty! discord @ "(Dusty) LoseThos#4303" \n\nYour choice is set to (db info here): \n1:Keep your choice as (db info here)\n2:Change your choice\nType Option Here: ')
+        # picker = input(f'Made by: Dusty! discord @ "(Dusty) LoseThos#4303" \n\nYour choice is set to (db info here): \nType Number For Option Here: ')
+        print('Made by: Dusty! discord @ "(Dusty) LoseThos#4303" \n ')
+        picker = input(
+                f'Your option choice is set to {prePickText}\n'
+                f'1:Keep {prePickText}\n'
+                f'2:Change settings/Choice\n'
+                f'Type Number Of Option Here: ')
+        picker = int(picker)
+    except:
+        system('cls||clear')
+        print(f'\n"{picker}" *is NOT a number please try again*')
+
+        startUI()
+    if picker == 1:
+        if prePickValue == 1:
+            useMouse = True  # will use mouse and right click to stop from logging out. Might be bugged if game not on main monitor
+        elif prePickValue == 2:
+            useArrowKey = True
+        elif prePickValue == 3:
+            useTyping = True
+        else:
+            useFKey = True
+        main()
+
+    elif picker == 2:
+        system('cls||clear')
+        changeUI()
+        useArrowKey = True
+    else:  # fail safe
+        system('cls||clear')
+        print(f'\n"{picker}" *is NOT an option to pick from. Please try again*')
+        startUI()
+
+def changeUI():
+    global useMouse, useArrowKey, useTyping, useFKey
+    picker = None
+    prePickValue, prePickText = readUsed()
+
+    try:
+        # quality = readQuality()
+        # print(f'Made by: Dusty! discord @ "(Dusty) LoseThos#4303" \n\nYour choice is set to (db info here): \n1:Keep your choice as (db info here)\n2:Change your choice\nType Option Here: ')
+        # picker = input(f'Made by: Dusty! discord @ "(Dusty) LoseThos#4303" \n\nYour choice is set to (db info here): \nType Number For Option Here: ')
+        print('Made by: Dusty! discord @ "(Dusty) LoseThos#4303" \n ')
+        picker = input(
+                f'1:Pick to use mouse *Recommended*\n'
+                f'2:Pick to use Arrow keys\n'
+                f'3:Pick to use chatbox typing\n'
+                f'4:Pick to use F keys\n'
+                f'5:Edit what f keys you want to use\n'
+                f'6:Edit what to type in chat box\n'
+                f'Type Number Of Option Here: ')
+        picker = int(picker)
+    except:
+        system('cls||clear')
+        print(f'\n"{picker}" *is NOT a number please try again*')
+
+        changeUI()
+
+    if picker is None:
+        # os.system('cls||clear')
+        print(f'\n{picker} *is NOT an option to pick from. Please try again*')
+        changeUI()
+
+    if picker == 1:
+        updateUsed(1, 'using Mouse', prePickValue, prePickText)
+        useMouse = True
+    elif picker == 2:
+        updateUsed(2, 'using Arrow Keys', prePickValue, prePickText)
+        useArrowKey = True
+    elif picker == 3:
+        updateUsed(3, 'using Typing', prePickValue, prePickText)
+        useTyping = True
+    elif picker == 4:
+        updateUsed(4, 'using F Keys', prePickValue, prePickText)
+        useFKey = True
+    elif picker == 5:
+        print("Sorry, does not work atm")
+        changeUI()
+    elif picker == 6:
+        print("Sorry, does not work atm")
+        changeUI()
+    else:  # fail safe
+        system('cls||clear')
+        print(f'\n"{picker}" *is NOT an option to pick from. Please try again*')
+        changeUI()
+    main()
 
 
 def main():
     global start
     start = time()
-    value = uniform(180, 270)  # 3min to 4.5min
-    value = 2
+    value = -1
 
     foundName = None
-
+    lastTime = None
+    lastX, lastY = 0, 0
     winID1 = FindWindow(None, f"RuneLite")
     winID2 = FindWindow(None, f"RuneLite - {name}")
     winID = None
@@ -75,20 +217,32 @@ def main():
         winID = winID2
     else:
         print("Could not find Runelite!\n"
-                        "Fix 1): Make sure your name is spelt exactly like in game.\n"
-                        '2) Go into Runelite plugin settings and turn off "Show display name in title"\n'
-                        '3) Make sure the game is open and running\n'
-                        '4) contact Dusty on discord @ "(Dusty) LoseThos#4303"')
+              'Fix 1): Make sure your name is spelt exactly like in game.\n'
+              '2) Go into Runelite plugin settings and turn off "Show display name in title"\n'
+              '3) Make sure the game is open and running\n'
+              '4) contact Dusty on discord @ "(Dusty) LoseThos#4303"')
         sleep(999999)
 
-    print("started")
+    on_button(lambda: timeReset(), buttons=['left', 'right'], types='up')
+    on_release(lambda _: timeReset())
+    print("Started!")
     start = time()
-    while not forceStop:
-        # while True:
 
-        on_button(lambda: timeReset(), buttons=['left', 'right'], types='up')
-        on_release(lambda _: timeReset())
+    while not forceStop:
+
         sleep(0.01)
+
+        timeValue = float(f'{(time() - start):.1f}')
+        if not timeValue == lastTime and timeValue % 1 == 0:
+            lastTime = timeValue
+            system('cls||clear')
+            print('Made by: Dusty! Discord @ "(Dusty) LoseThos#4303"\n')
+            print(f"Wait is set to: {(value / 60):.2f} min or {value:.2f} sec")
+
+            if useMouse:
+                print(f"Last Moved to [X: {lastX} | Y: {lastY}]")
+
+            print(f"\nAcivation in: {int(value - timeValue)} sec")
 
         if time() - start > value:
             changeBack = False
@@ -102,20 +256,24 @@ def main():
                 setWindow(winID)
 
                 changeBack = True
+            sleep(0.1)
+            if useArrowKey:
+                print("using a keys")
 
-            if useArrow:
                 keyList = ['up', 'down', 'left', 'right']
                 keyList = choice(keyList)
 
                 kpress(keyList)
                 sleep(0.02)
                 krelease(keyList)
-            elif useType:  # could combine useType and useFKey
+            elif useTyping:  # could combine useTyping and useFKey
+
                 for i in whatToType:
                     kpress(i)
                     sleep(0.02)
                     krelease(i)
             elif useFKey:
+
                 for i in fkeys:
                     kpress(i)
                     sleep(0.02)
@@ -137,6 +295,7 @@ def main():
 
                 posX, posY = get_position()
                 move(ranX, ranY, duration=0.02)
+                lastX, lastY = ranX, ranY
                 right_click()
                 move(posX, posY, duration=0.02)
 
@@ -144,7 +303,7 @@ def main():
                 setWindow(curWindowID)
 
             start = time()
-            value = uniform(180, 300)  # 3min to 4.5min
+            value = uniform(180, 290)  # 3min to 4.9min
 
 
-main()
+startUI()
